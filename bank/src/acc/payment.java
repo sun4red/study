af47045164e0454c25405256b9e1c9072ec4f8ea
sun4red <klzxcv1@gmail.com>
2023-08-07 20:23:41 +0900

    금액정산프로그램

---
+++ bank/src/acc/payment.java
@@ -0,0 +1,33 @@
+package acc;
+
+class payment {
+
+	private String name;
+	private String item;
+	private double amount;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+}
